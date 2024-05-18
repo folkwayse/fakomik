8c16e0e20f0ac585886619f614ce4a97d1f1a07e
Folkwayse <folkwayse@gmail.com>
2024-05-18 23:44:33 +0700

    nambah dikit

--- components/header.vue
+++ components/header.vue
@@ -26,7 +26,7 @@
           <template #end>
               <div class="flex items-center gap-2">
                   <InputText placeholder="Search" type="text" class="w-[8rem] sm:w-auto" />
-                  <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
+                  
               </div>
           </template>
       </Menubar>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <GenreSlider />
+  
   <MangaUpdate/>
 </template>
 
 <script setup>
 import MangaUpdate from "~/components/MangaUpdate.vue";
-import GenreSlider from '~/components/GenreSlider.vue';
+
 </script>
