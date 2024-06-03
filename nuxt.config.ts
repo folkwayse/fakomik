2dd78197f32c783f6a37f1510d5f2d87b157f635
Folkwayse <folkwayse@gmail.com>
2024-06-03 14:37:10 +0700

    fix histats

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,24 @@ interface Chapter {
 export default defineNuxtConfig({
   app: {
     head: {
+      script: [
+        {
+          innerHTML: `var _Hasync= _Hasync|| [];
+          _Hasync.push(['Histats.start', '1,4870396,4,0,0,0,00010000']);
+          _Hasync.push(['Histats.fasi', '1']);
+          _Hasync.push(['Histats.track_hits', '']);
+          (function() {
+          var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+          hs.src = ('//s10.histats.com/js15_as.js');
+          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+          })();`,
+          type: 'text/javascript'
+        }
+      ],
+      __dangerouslyDisableSanitizers: ['script'],
+      noscript: [
+        { innerHTML: `<a href="/" target="_blank"><img src="//sstatic1.histats.com/0.gif?4870396&101" alt="" border="0"></a>`, body: true }
+      ],
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
       link: [
@@ -21,9 +39,7 @@ export default defineNuxtConfig({
   },
 
   ssr: true,
-  plugins: [
-    '~/utils/histats.js'
-  ],
+ 
   devtools: { enabled: true },
   modules: [
     "@nuxtjs/sitemap",
@@ -36,9 +52,10 @@ export default defineNuxtConfig({
     "@pinia-plugin-persistedstate/nuxt",
     'nuxt-jsonld',
     '@nuxt/image',
-    'nuxt-cloudflare-analytics'
+    'nuxt-cloudflare-analytics',
     
   ],
+
   cloudflareAnalytics: {
     // See below for more options
     token: 'e269b38640754256852aa52657164770', // Example 1a2b3v4a5er6ac7r8afd
