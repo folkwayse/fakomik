63b20e526acae84e1e3fba1dc1985607e0d0f692
Folkwayse <folkwayse@gmail.com>
2024-06-20 13:46:42 +0700

    ganti popunder

--- utils/adsterra.js
+++ utils/adsterra.js
@@ -1,6 +1,6 @@
 export default function (app) {
     const script = document.createElement('script');
-    script.src = '//pl23588981.highrevenuenetwork.com/33/ce/c5/33cec54d946c2c9a13ba75119246b90f.js';
+    script.src = '//pl23589101.highrevenuenetwork.com/3d/d7/91/3dd791b2f8e1225cf3dee25f018eded9.js'; //popunder
     script.async = true;
   
     document.head.appendChild(script);
