3120ae9b6221c92af5812f09cad12bbbe5f9bc99
Yuda Pratama <folkwayse@gmail.com>
2024-11-24 09:11:28 +0700

    push

--- app.vue
+++ app.vue
@@ -2,9 +2,36 @@
 import HeaderComponent from "~/components/Header.vue";
 import FooterComponent from "~/components/Footer.vue";
 
-// useHead({
-//   meta: [{ name: "monetag", content: "455f676e09ad0ed36c2857340e16c7f5" }],
-// });
+useHead({
+  meta: [{ name: "monetag", content: "455f676e09ad0ed36c2857340e16c7f5" }],
+});
+
+useHead({
+  script: [
+    {
+      type: 'text/javascript',
+      src: '//pl23589101.profitablecpmrate.com/3d/d7/91/3dd791b2f8e1225cf3dee25f018eded9.js',
+      async: true, // Tambahkan jika script mendukung async
+    },
+  ],
+});
+
+useHead({
+  script: [
+    {
+      innerHTML: `(function(s,u,z,p){
+                    s.src=u,
+                    s.setAttribute('data-zone',z),
+                    p.appendChild(s);
+                  })(document.createElement('script'),
+                     'https://hoomigri.com/tag.min.js',
+                     7632577,
+                     document.body||document.documentElement);`,
+      type: 'text/javascript',
+    },
+  ],
+});
+
 </script>
 
 <template>
